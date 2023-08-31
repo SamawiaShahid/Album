7d011dc9c120911ba8643db5ef312ed5bf20062e
SamawiaShahid <125427056+SamawiaShahid@users.noreply.github.com>
2023-08-31 01:43:10 -0700

    working on album

--- src/app/albums/page.tsx
+++ src/app/albums/page.tsx
@@ -1,20 +1,24 @@
-
 import cloudinary from "cloudinary";
+import { AlbumCard } from "./album-card";
 
-export type SearchResult = {
-  public_id: string;
-  tags: string[];
-};
+export type Folder = { name: string; path: string };
 
-export default async function AlbumsPage(){
+export default async function AlbumsPage() {
+  const { folders } = (await cloudinary.v2.api.root_folders()) as {
+    folders: Folder[];
+  };
 
-const folders =await cloudinary.v2.api.root_folders()
-console.log(folders)
   return (
-    <section>;
+    <section>
       <div className="flex flex-col gap-8">
         <div className="flex justify-between">
           <h1 className="text-4xl font-bold">Albums</h1>
+        </div>
+
+        <div className="grid grid-cols-3 gap-4">
+          {folders.map((folder) => (
+            <AlbumCard key={folder.path} folder={folder} />
+          ))}
         </div>
       </div>
     </section>
